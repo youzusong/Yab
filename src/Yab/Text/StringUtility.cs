2927f03f77879ca3c2e0af9c5f743d9972ceedc3
youzusong <youzusong@outlook.com>
2021-11-04 13:07:12 +0800

    xxx

---
+++ src/Yab/Text/StringUtility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Web;
+
+namespace Yab.Text
+{
+    public static class StringUtility
+    {
+        public static string UrlDecode(string str)
+        {
+            return HttpUtility.UrlDecode(str);
+        }
+
+        public static string UrlEncode(string str, bool uppercase = false)
+        {
+            if (String.IsNullOrEmpty(str))
+                return str;
+
+            if (!uppercase)
+                return HttpUtility.UrlEncode(str);
+
+            var sb = new StringBuilder();
+            foreach (char c in str)
+            {
+                var encC = HttpUtility.UrlEncode(c.ToString());
+                sb.Append(encC.Length > 1 ? encC.ToUpper() : c);
+            }
+            return sb.ToString();
+        }
+
+    }
+}
